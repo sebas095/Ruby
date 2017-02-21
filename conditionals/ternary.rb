2ce006fc0449ba686443c2f402439b42e50b7fac
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-02-20 21:43:27 -0500

    ternary operator

---
+++ conditionals/ternary.rb
@@ -0,0 +1,18 @@
+user = 'Sebas'
+
+# if user == 'Sebas'
+#   puts 'Tutor'
+# else
+#   puts 'Visitante'
+# end
+
+# puts (if user == 'Sebas' then 'Tutor' else 'Visitante' end)
+# answer = if user == 'Sebas' then
+#   'Tutor'
+# else
+#   'Visitante'
+# end
+#
+# puts answer
+
+puts user == 'Sebas' ? 'Tutor' : 'Visitante'
