51d8a7d937d449a6384e739f12cabe2e8043b7fb
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-02-20 23:58:42 -0500

    ranges

---
+++ arrays/ranges.rb
@@ -0,0 +1,13 @@
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# puts numbers.min
+# puts numbers.max
+
+# (0..20).step(2).each do |number|
+#   puts number
+# end
+
+# ('a'..'z').each do |number|
+#   print number + ", "
+# end
+
+puts ('ma'..'md').to_a
