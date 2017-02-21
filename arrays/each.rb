be54f6de49f842790ef924a51994f5e09cd304d2
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-02-20 23:46:13 -0500

    arrays operations

--- arrays/each.rb
+++ arrays/each.rb
@@ -1,10 +1,19 @@
 # Ciclo each
 
-calificaciones = %w[10 7 8 9 5 8 10]
-accum = 0
+calificaciones = [10, 7, 8, 9, 5, 8, 10]
+# accum = 0
+#
+# calificaciones.each_with_index do |calificacion, index|
+#   accum += calificacion.to_f
+# end
 
-calificaciones.each_with_index do |calificacion, index|
-  accum += calificacion.to_f
-end
+# puts "El promedio de tus calificaciones es #{accum / calificaciones.size}"
 
-puts "El promedio de tus calificaciones es #{accum / calificaciones.size}"
+# .sort
+# .reverse
+# * '-' === .join('-')
+# .include?(6)
+# .first
+# .last
+# .uniq
+puts calificaciones.sample
