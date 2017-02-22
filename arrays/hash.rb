a2d1ce453520703794742fc112688a0d5e74260e
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-02-21 23:45:33 -0500

    hashes operations

--- arrays/hash.rb
+++ arrays/hash.rb
@@ -4,7 +4,23 @@
 # puts tutor[5]
 
 tutor = {nombre: 'Uriel', edad: 23, cursos: 10}
+user_info = {apellido: 'hernandez'}
+
 # puts tutor[:nombre]
-tutor.each do |key, value|
-  puts "En #{key} esta guardado #{value}"
-end
+# tutor.each do |key, value|
+#   puts "En #{key} esta guardado #{value}"
+# end
+
+# .size
+# .length
+# .has_key?()
+# .has_value?()
+# .keys
+# .values
+# .clear
+# .delete()
+# .empty?
+# .key()
+# .invert
+# .merge()
+puts tutor.merge(user_info)
